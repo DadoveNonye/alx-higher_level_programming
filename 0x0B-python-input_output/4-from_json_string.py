c6a31f42980f1e5d5b4aea8f019b78ea22ad9ef7
dadovenonye <chinonyenmecha@yahoo.com>
2023-11-09 20:50:16 -0800

    python-input_output

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import json
+
 """Function that retunrs a python data objrct"""
+import json
 def from_json_string(my_str):
     """returns an obj"""
     return json.loads(my_str)
