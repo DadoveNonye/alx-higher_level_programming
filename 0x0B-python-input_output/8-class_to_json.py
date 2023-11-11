27acbb02444d6275a1c8c988684d3979e3247301
dadovenonye <chinonyenmecha@yahoo.com>
2023-11-10 18:53:09 -0800

    python-input_output

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a dict desp with simple data structure"""
+"""Defines a dict desp with simple data structure."""
 def class_to_json(obj):
     """Returns the dictionary description with
-        simple data structure for JSON serialization of an object
+        simple data structure for JSON serialization of an object.
     """
     if hasattr(obj, "__dict__"):
         return obj.__dict__
