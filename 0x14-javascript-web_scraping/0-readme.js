2406f5eff6f6a562fcac6fa02029dbc29ecbd7c4
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-02-23 06:46:37 +0100

    fixed error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 
 const fs = require('fs');
 
-fs.readFile(filepath, 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
     if (err){
             console.log('error reading file');
         }else{
