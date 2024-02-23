3dbe77a43fa3df8b2d0e8d1aa857bb43016665d9
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-02-23 06:17:07 +0100

    reading file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// A  script that reads and prints the content of a file.
+
+const fs = require('fs')
+
+function read_filepath(filepath){
+    try{
+        fs.readFile(filepath, 'utf8', (err, data) => {
+        if (err){
+            console.log('error reading file');
+        }else{
+            console.log('FileContent :')
+            console(data)
+        }
+    });
+    }catch(err){
+        console.error('error reading file', err)
+    }
+
+}
+read_filepath()
