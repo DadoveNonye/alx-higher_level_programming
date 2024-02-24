7b766ae1fd6cd94a08f527a090cad8f34e1fb57e
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-02-24 07:13:45 +0100

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//A script that gets the contents of a webpage and stores it in a file.
+
+const request = require('request')
+
+request.get(process.argv[2], process.argv[3], {encoding: 'utf-8'}, (err, data) =>{
+    if (err){
+        console.error(err);
+    }else{
+        console.log(data)
+    }
+})
