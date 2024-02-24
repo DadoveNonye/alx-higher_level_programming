bf02bd5d7d3129dd3450ddd7c6ef3df930d9246f
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-02-24 02:35:31 +0100

    getting status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,6 +7,6 @@ request.get(process.argv[2], (error, response, body) => {
     if (error){
         console.log(error)
     }
-    statusCode = response.statusCode;
-    console.log('code :', {statusCode})
+    const statusCode = response;
+    console.log('code:', {statusCode})
 })
