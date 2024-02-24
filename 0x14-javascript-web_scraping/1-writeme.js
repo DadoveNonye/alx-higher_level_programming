6312cd9cfae5267dfc71eec55911876b0df1c688
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-02-24 01:44:31 +0100

    writing file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,10 +3,9 @@
 
 const fs = require('fs')
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
+fs.writeFile(process.argv[2], process.argv[3], {encoding: 'utf-8'}, (err) => {
     if (err){
         console.log(err);
         return;
     }
-    console.log(data)
 })
